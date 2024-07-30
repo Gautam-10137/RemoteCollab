85616183bd583be96f75429bcf81fc29b7a626ac
SimranGrak <simrangrak1761@gmail.com>
2024-07-30 03:51:50 -0400

    project started

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Nav from './Nav';
+import "./output.css";
+import "./input.css";
+import Register from './components/auth/Register';
+
+
+function App() {
+  return (
+   <>
+   <Nav/>
+  
+    <Register/>
+   
+   </>
+  );
+}
+
+export default App;
