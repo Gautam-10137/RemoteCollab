43effe37e60011dab814781dc3f90bdd2eef4174
Gautam-10137 <pahwagautam786@gmail.com>
2024-09-10 22:41:26 +0530

    Authentication Backend done

---
+++ server/routes/route.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+const {register,login}=require('../controller/AuthController');
+router.post("/auth/register",register);
+router.post("/auth/login",login);
+
+module.exports=router;
