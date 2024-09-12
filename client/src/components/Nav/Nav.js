d133af4cff048231f17d344eba617c30e0a70f0e
SimranGrak <simrangrak1761@gmail.com>
2024-09-11 23:38:07 -0400

    responsive design and navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    
-    <div>
-      <nav >
-     <div> 
-      <img src={require("../../asset/logo.png")} alt="logoimage"/></div>
-      <div ><p >Home</p></div>
-      <div ><p >About us</p></div>
-      <div ><p >Contact us</p></div>
-              <div>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-              </div>
+    <>
+      <nav className=" bg-slate-300 p-2">
+        <div className="flex relative w-full h-1container mx-auto flex justify-between items-center">
+          <div className="">
+            <img src={require("../../asset/logo.png")} alt="logoimage" />
+          </div>
+          <div className="flex text-center space-x-14 text-pink-700">
+            <p className="flex">Features
+            <img className="w-[16px] h-[16px] mt-[7px]" src={require("../../asset/down-arrow.png")} alt="down-arrowimage" />
+            </p>
+            <p>Resources</p>
+            <p>Demo</p>
+          </div>
+          <div className="flex space-x-4">
+            <Link className="bg-blue-200 w-[70px] text-center  bg-pink-500 text-white rounded-lg border border-gray-600" to="/login">
+              Login
+            </Link>
+            <Link className="bg-pink-200 w-[70px] text-center bg-pink-500 text-white rounded-lg border border-gray-600" to="/register">
+              Register
+            </Link>
+          </div>
+        </div>
       </nav>
-    </div>
+    </>
   );
 };
 
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -17,19 +17,19 @@ const Login = () => {
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const handlePasswordVisibility = ()=> {
+  const handlePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
 
-  const validateForm = ()=> {
+  const validateForm = () => {
     const newErrors = {};
-    if(!formData.name)newErrors.name = "userName is required";
-    if(!formData.email){
+    if (!formData.name) newErrors.name = "userName is required";
+    if (!formData.email) {
       newErrors.email = "Email is required";
-    } else if(!/\S+@\S+\.\S+/.test(formData.email)){
-        newErrors.email = "Email is invalid"
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Email is invalid";
     }
-    if(!formData.password)newErrors.password = "password is required";
+    if (!formData.password) newErrors.password = "password is required";
 
     return newErrors;
   };
@@ -57,83 +57,115 @@ const Login = () => {
   };
   return (
     <>
-    <div className="flex mt-[5px] ml-[450px] border-2 border-gray-400 w-[666px] h-[629px]">
-    <div className="bg-gradient-to-r from-pink-400 to-yellow-200 w-[333px] h-[629px]">
-        <div className=" mt-7  bg-transparent">
-      <img src={require("../../asset/logo.png")} alt="logoimage"/>
+      <div className="flex items-center justify-center md:mt-[40px] sm:mt-[20px]">
+        <div className="bg-gradient-to-r from-pink-400 to-yellow-200 w-[333px] h-[629px]">
+          <div className=" md:mt-7 sm:mt-4 lg:7 bg-transparent">
+            <img src={require("../../asset/logo.png")} alt="logoimage" />
+          </div>
+          <h1 className="font-bold text-4xl  text-center mt-[60px]  text-white">
+            Welcome Onboard
+          </h1>
+          <div className="lg:ml-[120px] lg:mt-[20px] md:ml-[120px] md:mt-[20px] sm:ml-[110px] sm:mt-[20px] items-end text-right">
+            <img src={require("../../asset/person.png")} alt="personimage" />
+          </div>
+          <p className="mt-[25px] md:text-left lg:text-center sm:text-right text-center lg:ml-[100px] md:ml-[100px] sm:ml-[70px] ml:[70px] mr-4 text-white lg:text-xl md:text-xl sm:text-xl text-xs">
+            Sign up and unlock new
+            <br></br>
+            experience!
+          </p>
+          <div className="flex lg:space-x-9 md:space-x-7 sm:space-x-6 space-x-4 mt-[70px]">
+            <p className="text-white ml-6">Follow:</p>
+            <img
+              className="w-6 h-6"
+              src={require("../../asset/linkedin.png")}
+              alt="linkedinimage"
+            />
+            <img
+              className="w-6 h-6"
+              src={require("../../asset/instagram.png")}
+              alt="instagramimage"
+            />
+            <img
+              className="w-6 h-6"
+              src={require("../../asset/facebook.png")}
+              alt="facebookimage"
+            />
+          </div>
+        </div>
+        <div className="bg-gradient-to-r from-slate-100 to-slate-300 w-[333px] h-[629px]">
+          <h1 className="font-bold text-3xl mt-9 text-center">Login</h1>
+          <div className="text-left lg:ml-9 md:ml-8 sm:ml-7 ml-6 lg:mt-9 md:mt-8 sm:mt-7 mt-6 lg:mr-4 md:mr-3 sm:mr-3 mr-6">
+            {!showForgotPassword ? (
+              <form onSubmit={handleSubmit}>
+                <br></br>
+
+                <br></br>
+                <br></br>
+                <label className="text-[14px]">Email:</label>
+                <br></br>
+                <input
+                  className="rounded-md w-[250px] border border-gray-400 mt-2"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInput}
+                />
+                <br></br>
+                <br></br>
+                <label className="text-[14px]">Password:</label>
+                <br></br>
+                <div className="relative flex items-center w-full">
+                <input
+                  className="rounded-md w-[250px] border border-gray-400 mt-2"
+                  type={isPasswordVisible ? "text" : "password"}
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInput}
+                />
+                    <button
+                  className="absolute lg:right-[40px] right-[10px] md:right-[45px] sm:right-[40px] cursor-pointer mt-[7px]"
+                  onClick={handlePasswordVisibility}
+                >
+                  {isPasswordVisible ? (
+                    <img
+                      className="w-4 h-4"
+                      src={require("../../asset/hide.png")}
+                      alt="hideimage"
+                    />
+                  ) : (
+                    <img
+                      className=" w-4 h-4"
+                      src={require("../../asset/show.png")}
+                      alt="showimage"
+                    />
+                  )}
+                </button>
+                </div>
+                
+                <button
+                  className="text-blue-600 text-[12px] lg:ml-40 md:ml-36 sm:ml-24 "
+                  type="submit"
+                  onClick={() => setShowForgotPassword(true)}
+                >
+                  Forgot Password?
+                </button>
+                <br></br>
+              
+                
+                <button
+                  className="text-center ml-16 mt-10 bg-black text-white w-[124px] h-[31px] text-sm rounded-2xl"
+                  type="submit"
+                >
+                  Login
+                </button>
+                <br></br>
+              </form>
+            ) : (
+              <ForgotPassword />
+            )}
+          </div>
+        </div>
       </div>
-       <h1 className="font-bold text-4xl text-center mt-[100px] text-white">Welcome Onboard</h1>
-       <div className="ml-[120px] mt-[20px]">
-       <img src={require("../../asset/person.png")} alt="personimage"/>
-       </div>
-       <p className="mt-[25px] text-left ml-[100px] mr-4 text-white text-xl">Login and unlock new
-        <br></br>
-       experience!</p>
-       <div className="flex space-x-9 mt-[70px]">
-        <p className="text-white ml-6">Follow:</p>
-        <img className="w-6 h-6" src={require("../../asset/linkedin.png")} alt="linkedinimage"/>
-        <img className="w-6 h-6" src={require("../../asset/instagram.png")} alt="instagramimage"/>
-        <img className="w-6 h-6" src={require("../../asset/facebook.png")} alt="facebookimage"/>
-       </div>
-      </div>
-      <div className="bg-gradient-to-r from-slate-100 to-slate-300 w-[333px] h-[629px]">
-      <h1 className="font-bold text-3xl mt-9 text-center">Login</h1>
-      <div className="text-left ml-9 mt-9">
-    {!showForgotPassword ? (
-      <form onSubmit={handleSubmit}>
-        <br></br>
-        <label className="text-[14px]">Name:</label>
-        <br></br>
-        <input className="rounded-md w-[250px] border border-gray-400 mt-2"
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleInput}
-          
-        />
-<br></br>
-<br></br>
-        <label className="text-[14px]">Email:</label>
-        <br></br>
-        <input className="rounded-md w-[250px] border border-gray-400 mt-2"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInput}
-          
-        />
-<br></br>
-<br></br>
-        <label className="text-[14px]">Password:</label>
-        <input className="rounded-md w-[250px] border border-gray-400 mt-2"
-          type={isPasswordVisible ? "text" :"password"}
-          name="password"
-          value={formData.password}
-          onChange={handleInput}
-        />
-       <br></br>
-       <button className="text-blue-600 text-[12px] ml-40" type="submit" onClick={()=> setShowForgotPassword(true)}>Forgot Password?</button>
-       <br></br>
-        <label className="text-sm mt-">
-          <input
-          type="checkbox"
-          checked={isPasswordVisible}
-          onChange={handlePasswordVisibility}
-          />
-          Show Password
-        </label>
-<br></br>
-<br></br>
-        <button className="text-center ml-16 mt-10 bg-black text-white w-[124px] h-[31px] text-sm rounded-2xl"  type="submit">Login</button>
-<br></br>
-       
-      </form>
-    ) : (
-      <ForgotPassword/>
-    )}
-    </div>
-    </div>
-     </div>
     </>
   );
 };
