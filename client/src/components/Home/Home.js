95719f20f64751faa0fcd360069f24980fc2125f
Alish2002 <alishkamboj2002@gmail.com>
2024-09-17 11:04:09 +0530

    home page completed

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -22,8 +22,44 @@ const Home = () => {
           <img src={require("../../asset/image1.png")} alt='image of home page' className="w-full h-auto sm:max-w-[300px] md:max-w-[350px] lg:max-w-[400px]"/>
          </div>
     </div>
+
+    {/* 2nd part */}
+    <div className=' bg-white  rounded-2xl mt-3 p-3 h-[700px]'>
+      <h1 className='ml-[300px] mt-8 text-[1.5rem] font-bold text-purple-600'>Why Choose RemoteCollab?</h1>
+<div className='flex items-center justify-center'>
+      <div className='bg-purple-100 border-2 text-center border-gray-700 rounded-md p-4 m-4 h-[250px] w-[300px]'>
+        <p className='text-[1rem] font-bold text-black'>Real-Time Chat</p>
+        <img className='p-4 ml-[33px] justify-center items-center ' src={require("../../asset/realTime.png")} alt='realTime chat'/>
+        <p className='text-[1rem]  text-black'>Stay Connected with your team Instantly.</p>
+      </div>
+      
+      <div className='bg-purple-100 border-2 text-center border-gray-700 rounded-md p-4 m-4 h-[250px] w-[300px]'>
+        <p className='text-[1rem] font-bold text-black'>Task & Project Management</p>
+        <img className='p-4 ml-[45px] justify-center items-center ' src={require("../../asset/TaskManage.png")} alt='realTime chat'/>
+        <p className='text-[1rem]  text-black'>Organize task, set deadlines and assign team members.</p>
+      </div>
+      <div className='bg-purple-100 border-2 text-center border-gray-700 rounded-md p-4 m-4 h-[250px] w-[300px]'>
+        <p className='text-[1rem] font-bold text-black'>File Sharing</p>
+        <img className='p-4 ml-[65px] justify-center items-center ' src={require("../../asset/fileSharing.png")} alt='realTime chat'/>
+        <p className='text-[1rem]  text-black'>Share important documents securely with your team.</p>
+      </div>
+</div>
+<div className='flex items-center justify-center'>
+      <div className='bg-purple-100 border-2 text-center border-gray-700 rounded-md p-4 m-4 h-[250px] w-[300px]'>
+        <p className='text-[1rem] font-bold text-black'>Analytics Dashboard</p>
+        <img className='p-4 ml-[50px] justify-center items-center ' src={require("../../asset/Analytical.png")} alt='realTime chat'/>
+        <p className='text-[1rem]  text-black'>Track Project Progress and key perfromances metrics.</p>
+      </div>
+      <div className='bg-purple-100 border-2 text-center border-gray-700 rounded-md p-4 m-4 h-[250px] w-[300px]'>
+        <p className='text-[1rem] font-bold text-black'>Integration</p>
+        <img className='p-4 ml-[70px] justify-center items-center ' src={require("../../asset/Integation.png")} alt='realTime chat'/>
+        <p className='text-[1rem]  text-black'>Sync with your calendar and other producticty tools.</p>
+      </div>
+</div>
+    </div>
     </>
   )
 }
+
 
 export default Home;
